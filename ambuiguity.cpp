51cac269bafd6e60f5a5516672a512e336315caf
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-08-07 09:54:47 +0530

    Update ambuiguity.cpp

--- ambuiguity.cpp
+++ ambuiguity.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 void print(int x) {
-  cout << "Integer: "<< x << endl;
+cout << "Integer: "<< x << endl;
 }
 void print(double x) {
 cout << "Double: " << x << endl;
 int main() {
-  print(5.5); // Ambiguity: 5.5 can be converted to both int and double
-
+print(5.5); // Ambiguity: 5.5 can be converted to both int and double
+return 0;
